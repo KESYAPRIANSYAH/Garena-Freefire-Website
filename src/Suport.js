4d09d9d3c86d6f1960193e8e66ed7620e1e173a9
kesy apriansyah <90308079+KESYAPRIANSYAH@users.noreply.github.com>
2022-06-15 22:23:28 +0700

    Update Suport.js

--- src/Suport.js
+++ src/Suport.js
@@ -2,13 +2,60 @@ function App() {
   return (
     <>
       <div class="bg-gray-300">
-        {/* <awal Jumbotron!> */}
-        <div clss="relative">
-          <div class="Jumbtron   text-center bg-cover bg-center bg-[url('ffbgcharacter.png')] ">
-            <div class="pb-8 shadow-lg rounded-lg text-gray-700 md:pb-80"></div>
+        {/* <slide!> */}
+
+        <div class="pt-14">
+          <div id="carouselExampleIndicators" class="carousel slide relative" data-bs-ride="carousel">
+            <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner relative w-full overflow-hidden">
+              <div class="carousel-item active float-left w-full">
+                <img src="https://dl.dir.freefiremobile.com/common/web_event/hash/b6a7383b4b6bf05be6c85d8b3d0a8555jpg" class="block w-full" alt="first" />
+                <div class="carousel-caption hidden xl:block absolute  text-left right-7s mb-20 ml-80">
+                  <h5 class="text-3xl text-amber-500 font-bold right-7 mb-5 ml-80">Moco "Enigma"</h5>
+                  <h6 class="right-7s  mb-5 ml-80">Moco is an outstanding hacker.</h6>
+                </div>
+              </div>
+              <div class="carousel-item float-left w-full">
+                <img src="https://dl.dir.freefiremobile.com/common/web_event/hash/972eba97ed66fe8abee6bb999ee39010jpg" class="block w-full" alt="dua" />
+                <div class="carousel-caption hidden xl:block absolute  text-left right-7s mb-20 ml-80">
+                  <h5 class="text-3xl text-amber-500 font-bold right-7 mb-5 ml-80">Kelly "The Swift"</h5>
+                  <h6 class="right-7s  mb-5 ml-80">Kelly is a high-school sprinter</h6>
+                </div>
+              </div>
+              <div class="carousel-item float-left w-full">
+                <img src="https://dl.dir.freefiremobile.com/common/web_event/hash/51e2f1c193eeda4666ba6e3144fd9d30jpg" class="block w-full" alt="tiga" />
+                <div class="carousel-caption hidden xl:block absolute  text-left right-7s mb-20 ml-80">
+                  <h5 class="text-3xl text-amber-500 font-bold right-7 mb-5 ml-80">Hayato "Firebra"</h5>
+                  <h6 class="right-7s  mb-5 ml-80">Hayato is a legendary Samurai</h6>
+                </div>
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="prev"
+            >
+              <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="next"
+            >
+              <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
           </div>
         </div>
-        {/* {/* < jumbotron> */}
+
+        {/* <akhir slider!> */}
 
         <div class="w-fullmx-auto pt-40 py-40 sm:px-40 px-10 ">
           <div class="p-6 border border-gray-500 sm:rounded-md">
